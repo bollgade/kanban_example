0a17c349fbe2401ff5352f1134994e039d218cd6
BLG_unix <bollgade@yandex.ru>
2023-09-17 12:41:05 +0300

    feat: add work with api [components/TaskDesk]

--- src/components/TaskDesk.jsx
+++ src/components/TaskDesk.jsx
@@ -1,24 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TaskList } from "./TaskList";
 import { statusMap } from "../consts/taskConsts";
+import axios from "axios";
+
+function getRightTaskFormat(task) {
+  const newTask = {
+    id: task.id,
+    team: task.userId,
+    title: task.title,
+    status: task.completed ? "Completed" : "ToDo",
+  };
+  return newTask;
+}
 
 export const TaskDesk = (props) => {
   const { className } = props;
   const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchTasks = async () => {
+    setIsLoading(true);
+    const { data } = await axios
+      .get("https://jsonplaceholder.typicode.com/todos?userId=2")
+      .then((res) => res)
+      .catch((e) => console.error(e))
+      .finally(() => setIsLoading(false));
+
+    const result = data.map(getRightTaskFormat);
+    setTasks(result);
+    return result;
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
   return (
     <div
       className={className}
       style={{ display: "flex", flexDirection: "row", gap: "15px" }}
     >
-      {Object.values(statusMap).map((status) => (
-        <TaskList
-          tasks={tasks}
-          setTasks={setTasks}
-          stage={status}
-          key={status}
-        />
-      ))}
+      {isLoading ? (
+        <p>LOADING...</p>
+      ) : (
+        <>
+          {Object.values(statusMap).map((status) => (
+            <TaskList
+              tasks={tasks}
+              setTasks={setTasks}
+              stage={status}
+              key={status}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
